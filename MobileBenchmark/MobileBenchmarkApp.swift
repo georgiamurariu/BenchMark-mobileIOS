b572a6c2a8d1472a059fc04d793e4278c4d6730a
GeorgiaMurariu <georgiamurariu@gmail.com>
2025-11-22 17:14:19 +0200

    Initial Commit

---
+++ MobileBenchmark/MobileBenchmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileBenchmarkApp.swift
+//  MobileBenchmark
+//
+//  Created by Murariu Georgiana-Roxana on 22.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct MobileBenchmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
